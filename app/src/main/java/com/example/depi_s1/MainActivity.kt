52bff2db3d828f4064fb180092fb629e01a6ff8a
Amir Hamdi <am3566146@gmail.com>
2024-12-20 16:45:11 +0200

    THIRD COMMENT AFTER THE NEW BRANCH

--- app/src/main/java/com/example/depi_s1/MainActivity.kt
+++ app/src/main/java/com/example/depi_s1/MainActivity.kt
@@ -9,13 +9,45 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
-    lateinit var button2: Button
+    lateinit var button : Button
+    lateinit var button2 : Button
     lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login_screen)
+        setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button6)
+        button2 = findViewById(R.id.button7)
+        textView = findViewById(R.id.textView)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button.setOnClickListener{
+
+            leftButtonClicked()
+        }
+
+        button2.setOnClickListener{
+
+            rightButtonClicked()
+        }
+
+    }
+
+    fun rightButtonClicked(){
+        textView.text = "Below button clicked"
+        button2.text = "clicked"
+        //ASDASFAWEWA
+        button.text = "Upper Button"
+    }
+
+    fun leftButtonClicked(){
+        textView.text = "Upper button clicked"
+        button.text = "clicked"
+        button2.text = "Below Button"
     }
 }
